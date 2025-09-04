1212934c70db4629e86608a256134066814a7953
Anchit Malhotra <anchit@Anchits-MacBook-Air.local>
2025-09-04 10:00:40 +0530

    Update social links to X and replace Twitter logo with X logo

--- app/footer.tsx
+++ app/footer.tsx
@@ -5,15 +5,10 @@ export function Footer() {
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
         Anchit Rites (
-        <A target="_blank" href="https://twitter.com/anchitrites">
+        <A target="_blank" href="https://x.com/anchitrites">
           @anchitrites
         </A>
         )
-      </div>
-      <div>
-        <A target="_blank" href="https://github.com/anchitrites/rites-blog">
-          Source
-        </A>
       </div>
     </footer>
   );
