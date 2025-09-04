efbf83363fccc35f97803045ed2475bcc28a3c9c
Anchit Malhotra <anchit@Anchits-MacBook-Air.local>
2025-09-04 10:24:18 +0530

    Scale X logo to match Follow me text size

--- app/header.tsx
+++ app/header.tsx
@@ -33,8 +33,8 @@ function XIcon(props: any) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={16}
-      height={16}
+      width={12}
+      height={12}
       viewBox="0 0 24 24"
       {...props}
     >
